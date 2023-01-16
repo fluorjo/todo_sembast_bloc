b3f63a1cafc8b9a4423b4158ef2ef4c3a838aee3
fluorjo <fluorjo@naver.com>
2023-01-16 22:35:09 +0900

    tododb 싱글톤 패턴

--- lib/data/todo_db.dart
+++ lib/data/todo_db.dart
@@ -1,1 +1,10 @@
+class TodoDb {
+  //싱글톤 클래스임. = 클래스가 단 하나의 인스턴스만 가지는 패턴이란 뜻.
+  static final TodoDb _singleton = TodoDb._internal();
+  //private internal 생성자
+  TodoDb._internal();
 
+  factory TodoDb() {
+    return _singleton;
+  }
+}
